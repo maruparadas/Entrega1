b631ece0b28edd1ef26e2139099478118e45ed0f
Andrea Vazquez <andreaceciliavazquez@yahoo.com.ar>
2022-03-28 21:09:19 -0300

    Formularios Completos

--- app_entrega1/urls.py
+++ app_entrega1/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from app_entrega1.views import fecha, autores, formulario_autores
+from app_entrega1.views import fecha, autores, formulario_autores, formulario_editorial, formulario_libros
 
 
 urlpatterns = [
     path('', fecha),
     path('autores/', autores, name = "Autores"),
-    path('autoresFormulario/', formulario_autores, name = "Formulario_Autores")
+    path('autoresFormulario/', formulario_autores, name = "Formulario_Autores"),
+    path('editorialFormulario/', formulario_editorial, name = "Formulario_Editorial"),
+    path('librosFormulario/', formulario_libros, name = "Formulario_Libros")
 ]
--- app_entrega1/views.py
+++ app_entrega1/views.py
@@ -2,8 +2,8 @@ from django.http.request import QueryDict
 from django.shortcuts import render, HttpResponse
 from django.http import HttpResponse
 from datetime import datetime
-from app_entrega1.models import Autores
-from app_entrega1.forms import AutorFormulario
+from app_entrega1.models import Autores, Editorial, Libros
+from app_entrega1.forms import AutorFormulario, EditorialFormulario, LibrosFormulario
 
 # Create your views here.
 
@@ -39,3 +39,51 @@ def formulario_autores(request):
         miFormulario = AutorFormulario()
 
     return render(request,"app_entrega1/autoresFormulario.html",{"miFormulario":miFormulario})    
+
+def formulario_editorial(request):
+    
+    if request.method == 'POST':
+
+        miFormulario = EditorialFormulario(request.POST)
+
+        print (miFormulario)
+
+        if miFormulario.is_valid:
+
+            data = miFormulario.cleaned_data
+
+            editorial = Editorial(data['nombre'],data['web'],data['pais_origen'])
+    
+            editorial.save()
+
+        return render(request,"app_entrega1/editorialFormulario.html") 
+
+    else:
+
+        miFormulario = EditorialFormulario()
+
+    return render(request,"app_entrega1/editorialFormulario.html",{"miFormulario":miFormulario})    
+
+def formulario_libros(request):
+    
+    if request.method == 'POST':
+
+        miFormulario = LibrosFormulario(request.POST)
+
+        print (miFormulario)
+
+        if miFormulario.is_valid:
+
+            data = miFormulario.cleaned_data
+
+            libros = Libros(data['isbn'],data['idioma'],data['titulo'],data['fecha_publicacion'],data['clasificacion'])
+    
+            libros.save()
+
+        return render(request,"app_entrega1/librosFormulario.html") 
+
+    else:
+
+        miFormulario = LibrosFormulario()
+
+    return render(request,"app_entrega1/librosFormulario.html",{"miFormulario":miFormulario})    
